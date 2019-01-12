fc0c7448e29ef28d4bd5aa68f645b02aaef01640
Max Howell <mxcl@me.com>
2019-01-11 23:01:44 -0500

    Add swift-sh

---
+++ swift-sh.rb
@@ -0,0 +1,15 @@
+class SwiftSh < Formula
+  desc "Scripting with easy zero-conf dependency imports"
+  homepage "https://github.com/mxcl/swift-sh"
+  url "https://github.com/mxcl/swift-sh", :using => :git 
+
+  bottle :unneeded
+
+  def install
+    system "swift", "build", 
+      "--configuration", "release",
+      "-Xswiftc", "-static-stdlib",
+      "--disable-sandbox"
+    bin.install '.build/release/swift-sh'
+  end
+end
