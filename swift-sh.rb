db8f288bf6d99c18f51702082b116bf60e6f87a2
Max Howell <mxcl@me.com>
2019-01-13 11:12:56 -0500

    v1.1.0
    Bottle removed as it didn’t help #1, and is likely not portable.

--- swift-sh.rb
+++ swift-sh.rb
@@ -1,14 +1,10 @@
 class SwiftSh < Formula
   desc "Scripting with easy zero-conf dependency imports"
   homepage "https://github.com/mxcl/swift-sh"
-  url "https://github.com/mxcl/swift-sh/archive/1.0.0.zip"
-  sha256 "57b32c7092e48dc46dcc0027551a00944545f2ab8e27d1550e2f4c58a278a82b"
+  url "https://github.com/mxcl/swift-sh/archive/1.1.0.zip"
+  sha256 "4567a01e828bd8df3949be45439edbb35a251fdc3a42c8b19fdfb935ec4743d6"
 
-  bottle do
-     root_url "https://github.com/mxcl/swift-sh/releases/download/1.0.0"
-     cellar :any_skip_relocation
-     sha256 "5fefe46b892693f45dceee8c5aea42f055468c60e914df9d1d05174a7b6bb3c3" => :mojave
-   end
+  bottle :unneeded
    
   def install
     system "swift", "build", 
