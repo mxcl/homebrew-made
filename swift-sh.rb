5875f86d24bef1d07e6c19f1ec5006d1cf72cdbe
Max Howell <mxcl@me.com>
2019-01-17 14:41:55 -0500

    v1.4.1

--- swift-sh.rb
+++ swift-sh.rb
@@ -1,8 +1,8 @@
 class SwiftSh < Formula
   desc "Scripting with easy zero-conf dependency imports"
   homepage "https://github.com/mxcl/swift-sh"
-  url "https://github.com/mxcl/swift-sh/archive/1.4.0.tar.gz"
-  sha256 "a4cf6ba2ec9fe751352d0508a0f66a483286fb41fee6357df770e250a04494b1"
+  url "https://github.com/mxcl/swift-sh/archive/1.4.1.tar.gz"
+  sha256 "695ebd6bb26b955f137bf845d6dd0f44eb587addf87ff460b73d87b684ce0b80"
 
   bottle :disabled
    
