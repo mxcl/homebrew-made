58f653eae49e4934d0b98b97b8d1bd0baf035282
Max Howell <mxcl@me.com>
2019-04-14 11:40:50 -0400

    Bottle for macOS >= 10.14.4

--- swift-sh.rb
+++ swift-sh.rb
@@ -4,10 +4,14 @@ class SwiftSh < Formula
   url "https://github.com/mxcl/swift-sh/archive/1.11.0.tar.gz"
   sha256 "1672abd8d479986a33e1b3a9ac6b86ad092729b393ff721cd4b7dae0030fb5df"
 
-  bottle :disabled
-   
+  bottle do
+    root_url "https://github.com/mxcl/swift-sh/releases/download/1.11.0"
+    cellar :any_skip_relocation
+    sha256 "50021a47caf3f24752039fb0d7d815c8a4a62011ec7e59ef70bb9cc99695d38e" => :mojave
+  end
+
   def install
-    args = ["swift", "build", 
+    args = ["swift", "build",
       "--configuration", "release",
       "--disable-sandbox"]
     args += ["-Xswiftc", "-static-stdlib"] unless swift_abi_safe or OS.linux?
@@ -21,7 +25,17 @@ class SwiftSh < Formula
   def swift_abi_safe
     # Swift 5 is ABI safe since Xcode 10.2-beta3
     return false unless OS.mac?
+    return false unless MacOS.full_version >= '10.14.4'
+    # this check is redundant really, but we’re just being careful
+    return false unless File.file? "/usr/lib/swift/libswiftFoundation.dylib"
     `swift --version` =~ /Swift version (\d+)\.\d+/
     $1.to_i >= 5
+  end
+
+  pour_bottle? do
+    reason "The bottle requires a Swift ABI stable version of macOS"
+    satisfy do
+      MacOS.version < '10.14.4'
+    end
   end
 end
