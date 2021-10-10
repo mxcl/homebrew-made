3945b55e789b94181544650a85ebbaa7fc5d74fd
Max Howell <mxcl@me.com>
2021-10-10 17:41:24 -0400

    wip

--- swift-sh.rb
+++ swift-sh.rb
@@ -4,6 +4,9 @@ class SwiftSh < Formula
   url "https://github.com/mxcl/swift-sh/archive/2.3.0.tar.gz"
   sha256 "512ab6ba0899258673e040fa434a2ee4332128eb6ae47f63b146477667ea2d83"
 
+  # brew devs suck nowadays
+  env: std
+
   def install
     args = ["swift", "build",
       "--configuration", "release",
