82bae2b2e204e30c6318379d4dc3c89ff25e1a59
Max Howell <mxcl@me.com>
2026-07-11 11:13:33 -0400

    git-cryobank 0.1.1

--- git-cryobank.rb
+++ git-cryobank.rb
@@ -1,8 +1,8 @@
 class GitCryobank < Formula
   desc "Archive Git repositories over SSH and browse them on the web"
   homepage "https://github.com/mxcl/git-cryobank"
-  url "https://github.com/mxcl/git-cryobank/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "08a1440056415ae8f16d4035e545f01d3345191a5e66348e9847783bada80bb6"
+  url "https://github.com/mxcl/git-cryobank/archive/refs/tags/v0.1.1.tar.gz"
+  sha256 "69a12b1078c8a50ad04dcedd0cf11822cd9e65b57ba727a43ea2627d225a1e5d"
 
   depends_on "go" => :build
 
