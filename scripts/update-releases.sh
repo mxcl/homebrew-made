67c6c9ea6590c41782528f9f1248a70336df2da9
Max Howell <mxcl@me.com>
2026-07-11 12:25:56 -0400

    Fix tap style checks

--- scripts/update-releases.sh
+++ scripts/update-releases.sh
@@ -3,33 +3,34 @@ set -euo pipefail
 
 root=${ROOT:-$(git rev-parse --show-toplevel)}
 api=(curl --fail --silent --show-error --location)
-if [[ -n ${GITHUB_TOKEN:-} ]]; then
-  api+=(--header "Authorization: Bearer $GITHUB_TOKEN")
+if [[ -n ${GITHUB_TOKEN:-} ]]
+then
+  api+=(--header "Authorization: Bearer ${GITHUB_TOKEN}")
 fi
 
 release() {
-  "${api[@]}" "https://api.github.com/repos/$1/releases/latest"
+  "${api[@]}" "https://api.github.com/repos/${1}/releases/latest"
 }
 
 checksum() {
-  "${api[@]}" "$1" | shasum -a 256 | cut -d ' ' -f 1
+  "${api[@]}" "${1}" | shasum -a 256 | cut -d ' ' -f 1
 }
 
 cryobank_release=$(release mxcl/git-cryobank)
-cryobank_tag=$(jq -r '.tag_name' <<<"$cryobank_release")
-cryobank_url="https://github.com/mxcl/git-cryobank/archive/refs/tags/$cryobank_tag.tar.gz"
-cryobank_sha=$(checksum "$cryobank_url")
-perl -0pi -e "s#url \"https://github.com/mxcl/git-cryobank/archive/refs/tags/[^\"]+\"\n  sha256 \"[^\"]+\"#url \"$cryobank_url\"\n  sha256 \"$cryobank_sha\"#" "$root/git-cryobank.rb"
+cryobank_tag=$(jq -r '.tag_name' <<<"${cryobank_release}")
+cryobank_url="https://github.com/mxcl/git-cryobank/archive/refs/tags/${cryobank_tag}.tar.gz"
+cryobank_sha=$(checksum "${cryobank_url}")
+perl -0pi -e "s#url \"https://github.com/mxcl/git-cryobank/archive/refs/tags/[^\"]+\"\n  sha256 \"[^\"]+\"#url \"${cryobank_url}\"\n  sha256 \"${cryobank_sha}\"#" "${root}/git-cryobank.rb"
 
 swift_release=$(release mxcl/swift-sh)
-swift_version=$(jq -r '.tag_name | sub("^v"; "")' <<<"$swift_release")
-swift_url="https://github.com/mxcl/swift-sh/archive/refs/tags/$swift_version.tar.gz"
-swift_sha=$(checksum "$swift_url")
-perl -0pi -e "s#url \"https://github.com/mxcl/swift-sh/archive/refs/tags/[^\"]+\"\n  sha256 \"[^\"]+\"#url \"$swift_url\"\n  sha256 \"$swift_sha\"#" "$root/swift-sh.rb"
+swift_version=$(jq -r '.tag_name | sub("^v"; "")' <<<"${swift_release}")
+swift_url="https://github.com/mxcl/swift-sh/archive/refs/tags/${swift_version}.tar.gz"
+swift_sha=$(checksum "${swift_url}")
+perl -0pi -e "s#url \"https://github.com/mxcl/swift-sh/archive/refs/tags/[^\"]+\"\n  sha256 \"[^\"]+\"#url \"${swift_url}\"\n  sha256 \"${swift_sha}\"#" "${root}/swift-sh.rb"
 
 pmm_release=$(release mxcl/package-manager-manager)
-pmm_version=$(jq -r '.tag_name | sub("^v"; "")' <<<"$pmm_release")
-pmm_url=$(jq -r --arg version "$pmm_version" '.assets[] | select(.name == "package-manager-manager-\($version).dmg") | .browser_download_url' <<<"$pmm_release")
-[[ -n $pmm_url ]]
-pmm_sha=$(checksum "$pmm_url")
-perl -0pi -e "s/version \"[^\"]+\"\n  sha256 \"[^\"]+\"/version \"$pmm_version\"\n  sha256 \"$pmm_sha\"/" "$root/Casks/package-manager-manager.rb"
+pmm_version=$(jq -r '.tag_name | sub("^v"; "")' <<<"${pmm_release}")
+pmm_url=$(jq -r --arg version "${pmm_version}" '.assets[] | select(.name == "package-manager-manager-\($version).dmg") | .browser_download_url' <<<"${pmm_release}")
+[[ -n ${pmm_url} ]]
+pmm_sha=$(checksum "${pmm_url}")
+perl -0pi -e "s/version \"[^\"]+\"\n  sha256 \"[^\"]+\"/version \"${pmm_version}\"\n  sha256 \"${pmm_sha}\"/" "${root}/Casks/package-manager-manager.rb"
--- swift-sh.rb
+++ swift-sh.rb
@@ -8,25 +8,9 @@ class SwiftSh < Formula
   env :std unless OS.mac?
 
   def install
-    args = ["swift", "build",
-      "--configuration", "release",
-      "--disable-sandbox"]
-    args += ["-Xswiftc", "-static-stdlib"] unless swift_abi_safe or OS.linux?
+    system "swift", "build", "--configuration", "release", "--disable-sandbox"
 
-    system *args
-
-    bin.install '.build/release/swift-sh'
-    bin.install '.build/release/swift-sh-edit' if OS.mac?
-  end
-
-  def swift_abi_safe
-    # Swift 5 is ABI safe since Xcode 10.2-beta3
-    return false unless OS.mac?
-    return true if MacOS.version >= 11
-    return false unless MacOS.full_version >= '10.14.4'
-    # this check is redundant really, but we’re just being careful
-    return false unless File.file? "/usr/lib/swift/libswiftFoundation.dylib"
-    `swift --version` =~ /Swift version (\d+)\.\d+/
-    $1.to_i >= 5
+    bin.install ".build/release/swift-sh"
+    bin.install ".build/release/swift-sh-edit" if OS.mac?
   end
 end
